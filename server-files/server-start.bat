75f2231695f92f5966d1cb76f52f95dc4641761b
Lance Peters <59713801+agentmealoaf@users.noreply.github.com>
2020-08-17 10:31:50 -0700

    change da batchfile

--- server-files/server-start.bat
+++ server-files/server-start.bat
@@ -7,16 +7,17 @@ echo Installing Java...
 goto getjava
 )
 
+:getjava
+powershell -Command "Invoke-WebRequest https://download3.portableapps.com/portableapps/Java64/jPortable64_8_Update_241_online.paf.exe?20190321 -OutFile ..\java\jportable_online.paf.exe"
+..\java\jportable_online.paf.exe
+goto 1
+
 :1a
 if exist "%~dp0tuinity-paperclip.jar" (
 echo Tuinity is already downloaded!
 ) else (
-goto tuchoose
+goto tutu
 )
-
-findstr "eula=true" eula.txt>NUL
-if errorlevel 1 (goto notfound) else (goto found)
-goto :eof
 
 :tutu
 echo Tuinity will now be downloaded
@@ -24,37 +25,33 @@ powershell -Command "Invoke-WebRequest https://ci.codemc.io/job/Spottedleaf/job/
 :1b
 if exist "%~dp0tuinity-paperclip.jar" (
 echo Tuinity was sucessfully downloaded!
-pause
 ) else (
 echo Tuinity was unable to be downloaded, check the link
 pause
+exit
 )
+
+findstr "eula=true" eula.txt>NUL
+if errorlevel 1 (goto notfound) else (goto runserver)
+goto :eof
 
 :notfound
 set /p accept="Accept EULA? https://www.minecraft.net/en-us/eula (y/n): "
 if /i "%accept%" == "y" (goto writetrue) else (goto wellthen)
 goto :eof
 
-:found
-
 :writetrue
 echo eula=true> eula.txt
 goto runserver
 
-:wellthen
-echo No server for you then
-pause
-exit
-
-:tuchoose
-set /p tudownload="Tuinity needs to be downloaded, would you like to do this now? (y/n): "
-if /i "%tudownload%" == "y" (goto tutu) else (goto wellthen)
-
-:getjava
-powershell -Command "Invoke-WebRequest https://download3.portableapps.com/portableapps/Java64/jPortable64_8_Update_241_online.paf.exe?20190321 -OutFile ..\java\jportable_online.paf.exe"
-..\java\jportable_online.paf.exe
-goto 1
-
 :runserver
 "%~dp0..\java\CommonFiles\Java64\bin\java.exe" -jar -Xmx1536m -Xms512m tuinity-paperclip.jar nogui
 pause
+
+:wellthen
+echo You must accept the EULA to continue...
+pause
+exit
+
+
+
