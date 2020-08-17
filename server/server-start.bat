e747e17fc14ae69b1c3057d4dffaef4010b14bb2
Lance Peters <59713801+agentmealoaf@users.noreply.github.com>
2020-08-17 15:04:05 -0700

    Update server-start.bat

--- server/server-start.bat
+++ server/server-start.bat
@@ -9,16 +9,12 @@
     ) else (
        goto InstallPaper
        )
- 
-goto CommonExit
 
 @rem Downloads the latest build of paper from the jenkins
 :InstallPaper
     echo Downloading Paper...
     powershell -Command "Invoke-WebRequest https://papermc.io/ci/job/Paper-1.16/lastSuccessfulBuild/artifact/paperclip.jar -OutFile paperclip.jar"
     goto CheckPaper
-
-goto CommonExit
 
 @rem Checks if a server.properties file has already been generated, if not (on first run) it creates one and add custom motd
 :CheckServerProperties
@@ -31,20 +27,20 @@ goto CommonExit
        goto CheckEula
        )
 
-goto CommonExit
-
 @rem Checks if a eula.txt has been generated, if not (on first run) it creates an empty text document
 :CheckEula
     if exist "eula.txt" (
        findstr "eula=true" eula.txt>NUL
-       if errorlevel 1 (goto SetEula) else (goto StartServer)
+       if errorlevel 1 (goto SetEula) else (
+           echo EULA Accepted...
+           goto StartServer
+           )
 
     ) else (
        break>"eula.txt"
        goto SetEula
        )
 
-goto CommonExit
 
 @rem User input to accept, decline, and read the eula
 :SetEula
@@ -52,22 +48,19 @@ goto CommonExit
        if /i "%accept%" == "y" (goto AcceptEula)
        if /i "%accept%" == "n" (goto DeclineEula)
        if /i "%accept%" == "read" (goto ReadEula)
-    
-goto CommonExit
+
 
 @rem Opens eula page in browser
 :ReadEula
     start "" https://www.minecraft.net/en-us/eula
     goto SetEula
 
-goto CommonExit
 
 @rem Accepts eula
 :AcceptEula
     echo eula=true> eula.txt
+    echo EULA Accepted...
     goto StartServer
-
-goto CommonExit
 
 @rem Doesn't actually delcine the eula, warns the user and returns to :SetEula
 :DeclineEula
@@ -75,16 +68,16 @@ goto CommonExit
     pause
     goto SetEula
 
-goto CommonExit
-
 @rem User input to choose min and max ram, followed by java command to start the server. Portable Java WIP
 :StartServer
     set /p RamMin="Enter Minimum Ram Ammount (mb): "
+       if /i "%RamMin%" LSS "512" (
+           echo You need to allocate at least 512mb
+           goto StartServer
+       )
     set /p RamMax="Enter Maximum Ram Ammount (mb): "
 
     java -jar -Xmx%RamMax%m -Xms%RamMin%m paperclip.jar nogui
-
-goto CommonExit
 
 @rem A common exit to point to 
 :CommonExit
